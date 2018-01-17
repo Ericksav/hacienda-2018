8feec96aaea02362727fedbd6f6c219b56c88d78
Erick Alva <erick.vlasco@gmail.com>
2018-01-16 16:43:08 -0800

    start

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+var webpack = require('webpack');
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var ImageminPlugin = require('imagemin-webpack-plugin').default;
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+      rules:
+      [
+      	{
+          test: /\.(sass|scss)$/,
+          use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+              loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('postcss-cssnext')(),
+              ]
+            }
+          },
+          {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
+        },
+        {
+          test: /\.(jpe?g|png|gif)$/i,
+          loader:"file-loader",
+          query:{
+            name:'[name].[ext]',
+            outputPath:'img/'
+            //the images will be emmited to public/assets/images/ folder 
+            //the images will be put in the DOM <style> tag as eg. background: url(assets/images/image.png); 
+          }
+        }
+      ]
+    },
+  plugins: [
+    new BrowserSyncPlugin({
+    // browse to http://localhost:8000/ during development, 
+    // ./public directory is being served 
+      host: 'localhost',
+      port: 8000,
+      server: './',
+      files: [
+          "./src/*.js",
+          "./*.html",
+          "./src/scss/**/*.scss"
+        ]
+    }),
+    // Copy the images folder and optimize all the images
+    new CopyWebpackPlugin([
+      {
+        from: 'src/img/',
+        to: './img'
+      }
+    ]),
+    // new ImageminPlugin({
+    //   disable: process.env.NODE_ENV !== 'production', // Disable during development
+    //   pngquant: {
+    //     quality: '60-70'
+    //   }
+    // })
+    new ImageminPlugin(
+    	{
+    		disable: process.env.NODE_ENV !== 'production',
+        test: /\.(jpe?g|png|gif|svg)$/i
+    	}
+    )
+  ]
+};
+
+
