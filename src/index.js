76bb08ab8dec736f42010eba336db4367650aa0e
Erick Alva <ericksav@Ericks-iMac.local>
2018-08-31 17:13:48 -0700

    update forms y modal

--- src/index.js
+++ src/index.js
@@ -7,5 +7,36 @@ console.log('Si Sirvo');
 Vue.use(VeeValidate);
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+		visibleContact: false
+	},
+  methods: {
+    validateBeforeSubmit() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          // eslint-disable-next-line
+          alert('Format submitted');
+          return;
+        }
+
+        alert('Please fill out all the required fields');
+      });
+    },
+		showContact() {
+			this.visibleContact = !this.visibleContact;
+		},
+		hideContact() {
+			this.visibleContact = !this.visibleContact;
+		}
+  }
 });
+
+
+
+//FORM DATE
+let today = new Date().toISOString().substr(0, 10);
+document.querySelector(".today").value = today;
+
+let next = new Date().toISOString().substr(0, 10);
+document.querySelector(".next").value = this.value = next;
