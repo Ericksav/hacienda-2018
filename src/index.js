715f6a8a908d3f8fc4bae929ed3980ac7a1f85cd
Erick Alva <ericksav@Ericks-iMac.local>
2018-09-03 09:37:18 -0700

    udpate component nav

--- src/index.js
+++ src/index.js
@@ -29,7 +29,39 @@ new Vue({
 		hideContact() {
 			this.visibleContact = !this.visibleContact;
 		}
-  }
+  },
+});
+
+
+Vue.component('form-contact', {
+  template: '<header>
+		<div class="c-border u-hide__small"></div>
+		<div class="o-flex o-flex--middle c-header">
+			<div class="c-header__logo">
+				<a href="/"><img src="/dist/img/logo-header.png" alt=""></a>
+			</div>
+			<div class="c-header__menu-small">
+				<a href="">MENU</a>
+			</div>
+			<div class="c-header__nav">
+				<div class="c-header__links">
+					<ul class="o-flex u-mxa">
+						<li><a href="/rooms/">Rooms & Services</a></li>
+						<li><a href="/restaurant/">Restaurant</a></li>
+						<li><a href="/gallery/">Gallery</a></li>
+						<li><a href="/promotions/">Promotions</a></li>
+						<li><a href="/#location-home">Location</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="c-header__cta">
+				<span>REST IN BEAUTIFUL MEXICO</span>
+				<button v-on:click="showContact" class="c-button-general c-button-general__cta">Book a Room</button>
+			</div>
+		</div>
+		<div class="c-border u-show__small"></div>
+
+	</header>'
 });
 
 
